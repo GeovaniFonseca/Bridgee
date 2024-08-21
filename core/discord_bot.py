0e5eefd8a2f28f815b4d4c05a52df892b8ac3156
GeovaniFonseca <116107463+GeovaniFonseca@users.noreply.github.com>
2024-08-21 14:50:44 -0300

    Update discord_bot.py

--- core/discord_bot.py
+++ core/discord_bot.py
@@ -333,12 +333,12 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                         if "]:" in message:
                             memberusername = message.split()[1]
                         else:
-                            memberusername = message.split()[1][:-1]
+                            memberusername = message.split()[1][:-1] + "]"  # Adicionado colchete de fechamento
                     else:
                         if "]:" in message:
                             memberusername = message.split()[0]
                         else:
-                            memberusername = message.split()[0][:-1]
+                            memberusername = message.split()[0][:-1] + "]"  # Adicionado colchete de fechamento
                     if self.mineflayer_bot.bot.username in memberusername:
                         return
                     if " joined." in message:
@@ -358,10 +358,10 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                     else:
                         username = username.split(" ")[0]
                     username = username.strip()
-    
+
                     self.dispatch("hypixel_guild_message", username, message)
                     await self.send_user_message(username, message)
-    
+
             elif message.startswith("Officer >"):
                 channel = self.get_channel(DiscordConfig.officerChannel)
                 if channel is None:
@@ -371,28 +371,28 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                     return
                 message = message.replace("Guild >", "")
                 if "[" in username:
-                    username = username.split("]")[1]
+                    username = username.split("]")[1] + "]"  # Adicionado colchete de fechamento
                 username = username.strip()
                 self.dispatch("hypixel_guild_officer_message", username, message)
                 await self.send_user_message(username, message, officer=True)
-    
+
             # Bot recieved guild invite
             elif "Click here to accept or type /guild accept " in message:
                 if "[VIP]" in message or "[VIP+]" in message or "[MVP]" in message or "[MVP+]" in message or "[MVP++]" in message:
                     playername = message.split()[2]
                 else:
                     playername = message.split()[1]
-    
+
                 embed = Embed(timestamp=discord.utils.utcnow(), colour=0x1ABC9C)
                 embed.set_author(
                     name=f"{playername} invited me to a guild!",
                     icon_url="https://www.mc-heads.net/avatar/" + playername
                 )
-    
+
                 self.dispatch("hypixel_guild_invite_recieved", playername)
                 await self.send_debug_message("Sending invite recieved message")
                 await self.send_message(embed=embed)
-    
+
             # Someone joined/left the guild
             elif " joined the guild!" in message:
                 message = message.split()
@@ -420,7 +420,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_member_leave", playername)
                 await self.send_debug_message("Sending guild member left message")
                 await self.send_message(embed=embed)
-    
+
             # Someone was promoted/demoted
             elif " was promoted from " in message:
                 message = message.split()
@@ -454,7 +454,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_member_demote", playername, from_rank, to_rank)
                 await self.send_debug_message("Sending member demoted message")
                 await self.send_message(embed=embed)
-    
+
             # Someone was kicked
             elif " was kicked from the guild!" in message:
                 message = message.split()
@@ -486,7 +486,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_member_kick", playername)
                 await self.send_debug_message("Sending member kicked message")
                 await self.send_message(embed=embed)
-    
+
             # Join/leave notifications toggled
             elif "Disabled guild join/leave notifications!" in message:
                 embed = Embed(description="Disabled guild join/leave notifications!", colour=0x1ABC9C)
@@ -496,21 +496,21 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 embed = Embed(description="Enabled guild join/leave notifications!", colour=0x1ABC9C)
                 await self.send_debug_message("Sending notification enabled message")
                 await self.send_message(embed=embed)
-    
+
             # Hypixel antispam filter
             elif "You cannot say the same message twice!" in message:
                 embed = Embed(description="You cannot say the same message twice!", colour=0x1ABC9C)
                 self.dispatch("hypixel_guild_message_send_failed", message)
                 await self.send_debug_message("Sending 'same message twice' message")
                 await self.send_message(embed=embed)
-    
+
             # Bot cannot access officer chat
             elif "You don't have access to the officer chat!" in message:
                 embed = Embed(description="You don't have access to the officer chat!", colour=0x1ABC9C)
                 self.dispatch("hypixel_guild_message_send_failed", message)
                 await self.send_debug_message("Sending no officer chat access message")
                 await self.send_message(embed=embed)
-    
+
             # Bot invited someone
             elif (
                     ("You invited" in message and "to your guild. They have 5 minutes to accept." in message)
@@ -536,7 +536,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_member_invite", playername)
                 await self.send_debug_message("Sending invite sent message")
                 await self.send_message(embed=embed)
-    
+
             # Person bot invited is in another guild
             elif " is already in another guild!" in message:
                 message = message.split()
@@ -553,7 +553,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_member_invite_failed", playername)
                 await self.send_debug_message("Sending invited player in guild message")
                 await self.send_message(embed=embed)
-    
+
             # Person bot invited is in already in the guild
             elif " is already in your guild!" in message:
                 message = message.split()
@@ -570,7 +570,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_member_invite_failed", playername)
                 await self.send_debug_message("Sending invited player in our guild message")
                 await self.send_message(embed=embed)
-    
+
             # Person bot invited has guild invites disabled (can't figure out who)
             elif "You cannot invite this player to your guild!" in message:
                 embed = Embed(timestamp=discord.utils.utcnow(), colour=0x1ABC9C)
@@ -581,7 +581,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_member_invite_failed", None)
                 await self.send_debug_message("Sending invites disabled message")
                 await self.send_message(embed=embed)
-    
+
             # Person bot invited has already been invited
             elif "You've already invited" in message and "to your guild! Wait for them to accept!" in message:
                 message = message.split()
@@ -597,7 +597,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_member_invite_failed", None)
                 await self.send_debug_message("Sending invites disabled message")
                 await self.send_message(embed=embed)
-    
+
             elif "Your guild is full!" in message:
                 embed = Embed(colour=0x1ABC9C)
                 embed.set_author(
@@ -607,7 +607,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_member_invite_failed", None)
                 await self.send_debug_message("Sending guild full message")
                 await self.send_message(embed=embed)
-    
+
             # mute stuff
             elif "has muted the guild chat" in message:
                 splitmsg = message.split()
@@ -624,7 +624,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_chat_muted", playername, duration)
                 await self.send_debug_message("Sending guild chat muted message")
                 await self.send_message(embed=embed)
-    
+
             elif "has unmuted the guild chat" in message:
                 splitmsg = message.split()
                 playername = splitmsg[0]
@@ -639,7 +639,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 self.dispatch("hypixel_guild_chat_unmuted", playername)
                 await self.send_debug_message("Sending guild chat unmuted message")
                 await self.send_message(embed=embed)
-    
+
             # personal mutes
             elif "has muted" in message:
                 _regex = r"(?:\[[A-Z+]+\] )?([a-zA-Z0-9_]+) has muted (?:\[[A-Z+]+\] )?([a-zA-Z0-9_]+) for ([a-zA-Z0-9_]+)"
@@ -655,7 +655,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 )
                 await self.send_debug_message("Sending member muted message")
                 await self.send_message(embed=embed, officer=True)
-    
+
             elif "has unmuted" in message:
                 _regex = r"(?:\[[A-Z+]+\] )?([a-zA-Z0-9_]+) has unmuted (?:\[[A-Z+]+\] )?([a-zA-Z0-9_]+)"
                 matches = re.match(_regex, message)
@@ -669,7 +669,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 )
                 await self.send_debug_message("Sending member unmuted message")
                 await self.send_message(embed=embed, officer=True)
-    
+
             elif "You're currently guild muted for" in message:
                 duration_remaining = message.split()[-1][:-1]
                 self.dispatch("hypixel_guild_message_send_failed")
@@ -679,79 +679,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 )
                 await self.send_debug_message("Sending bot is guild muted message")
                 await self.send_message(embed=embed)
-    
-            # mute stuff
-            elif "has muted the guild chat" in message:
-                splitmsg = message.split()
-                playername = splitmsg[0]
-                if ("[VIP]" in splitmsg or "[VIP+]" in splitmsg or
-                        "[MVP]" in splitmsg or "[MVP+]" in splitmsg or "[MVP++]" in splitmsg):
-                    playername = splitmsg[1]
-                duration = splitmsg[-1]
-                embed = Embed(colour=0x1ABC9C)
-                embed.set_author(
-                    name=f"The guild chat has been muted by {playername} for {duration}.",
-                    icon_url="https://www.mc-heads.net/avatar/" + playername
-                )
-                self.dispatch("hypixel_guild_chat_muted", playername, duration)
-                await self.debug_webhook.send("guild-muted")
-                await self.send_message(embed=embed)
-    
-            elif "has unmuted the guild chat" in message:
-                splitmsg = message.split()
-                playername = splitmsg[0]
-                if ("[VIP]" in splitmsg or "[VIP+]" in splitmsg or
-                        "[MVP]" in splitmsg or "[MVP+]" in splitmsg or "[MVP++]" in splitmsg):
-                    playername = splitmsg[1]
-                embed = Embed(colour=0x1ABC9C)
-                embed.set_author(
-                    name=f"The guild chat has been unmuted by {playername}.",
-                    icon_url="https://www.mc-heads.net/avatar/" + playername
-                )
-                self.dispatch("hypixel_guild_chat_unmuted", playername)
-                await self.debug_webhook.send("guild-unmuted")
-                await self.send_message(embed=embed)
-    
-            # personal mutes
-            elif "has muted" in message:
-                _regex = r"(?:\[[A-Z+]+\] )?([a-zA-Z0-9_]+) has muted (?:\[[A-Z+]+\] )?([a-zA-Z0-9_]+) for ([a-zA-Z0-9_]+)"
-                matches = re.match(_regex, message)
-                muter = matches.group(1)
-                muted = matches.group(2)
-                duration = matches.group(3)
-                self.dispatch("hypixel_guild_member_muted", muter, muted, duration)
-                embed = Embed(colour=0x1ABC9C)
-                embed.set_author(
-                    name=f"{muter} has muted {muted} for {duration}.",
-                    icon_url="https://www.mc-heads.net/avatar/" + muter
-                )
-                await self.debug_webhook.send("guild-member-mute")
-                await self.send_message(embed=embed, officer=True)
-    
-            elif "has unmuted" in message:
-                _regex = r"(?:\[[A-Z+]+\] )?([a-zA-Z0-9_]+) has unmuted (?:\[[A-Z+]+\] )?([a-zA-Z0-9_]+)"
-                matches = re.match(_regex, message)
-                muter = matches.group(1)
-                muted = matches.group(2)
-                self.dispatch("hypixel_guild_member_unmuted", muter, muted)
-                embed = Embed(colour=0x1ABC9C)
-                embed.set_author(
-                    name=f"{muter} has unmuted {muted}.",
-                    icon_url="https://www.mc-heads.net/avatar/" + muter
-                )
-                await self.debug_webhook.send("guild-member-unmute")
-                await self.send_message(embed=embed, officer=True)
-    
-            elif "You're currently guild muted for" in message:
-                duration_remaining = message.split()[-1][:-1]
-                self.dispatch("hypixel_guild_message_send_failed")
-                embed = Embed(colour=0x1ABC9C)
-                embed.set_author(
-                    name=f"The bot is currently guild muted for {duration_remaining}.",
-                )
-                await self.debug_webhook.send("bot-guild-muted")
-                await self.send_message(embed=embed)
-    
+
             # /g list command response
             elif "Total Members:" in message:
                 message = re.split("--", message)
@@ -766,7 +694,7 @@ async def _send_message(self, *args, **kwargs) -> Union[discord.Message, discord
                 embed = Embed(description=embed.replace("_", "\\_"), colour=0x1ABC9C)
                 await self.send_debug_message("Sending player list message")
                 await self.send_message(embed=embed)
-    
+
             # Everything else is sent as a normal message
             else:
                 if message.strip() == "":
